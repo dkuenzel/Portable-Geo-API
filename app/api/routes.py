fc3ada576964c190dcf60c94a2558101d0ab68c5
dkuenzel <david.kuenzel@trivago.com>
2018-12-18 17:56:46 +0100

    added route for distance output

--- app/api/routes.py
+++ app/api/routes.py
@@ -3,6 +3,13 @@ from flask import jsonify
 from lib.basic_types import *
 from lib.requests import *
 
+## Allow negative values in request
+from werkzeug.routing import FloatConverter as BaseFloatConverter
+class FloatConverter(BaseFloatConverter):
+    regex = r'-?\d+(\.\d+)?'
+app.url_map.converters['float'] = FloatConverter
+
+## Routes
 # Output help for requests on root
 @app.route('/')
 def help():
@@ -29,3 +36,10 @@ def getRawRoute(origin_lon, origin_lat, destination_lon, destination_lat):
 	request = geoRequest(origin_lon, origin_lat, destination_lon, destination_lat)
 	request.p2p()
 	return jsonify(request.getRaw())
+	
+# Try: http://127.0.0.1:5000/p2p/2/5.125/51.31234/5.12/51.31
+@app.route('/p2p/2/<float:origin_lon>/<float:origin_lat>/<float:destination_lon>/<float:destination_lat>', methods=['GET'])
+def getRouteDistance(origin_lon, origin_lat, destination_lon, destination_lat):
+	request = geoRequest(origin_lon, origin_lat, destination_lon, destination_lat)
+	request.p2p()
+	return jsonify(request.getDistance())
