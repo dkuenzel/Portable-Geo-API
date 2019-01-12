0f50780d1cd72263e7ea5a3feeda457a4c9aae73
dkuenzel <45232239+dkuenzel@users.noreply.github.com>
2019-01-12 02:13:09 +0100

    folder structure clean up

--- app/static/vendor/accordion.js
+++
@@ -1,9 +0,0 @@
-jQuery(function() {
-	jQuery('.ss_button').on('click',function() {
-		jQuery('.ss_content').slideUp('fast');
-		content = jQuery(this).next('.ss_content');
-		if (content.is(":hidden")) {
-			jQuery(this).next('.ss_content').slideDown('fast');
-		}
-	});
-});
