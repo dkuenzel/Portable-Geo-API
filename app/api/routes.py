6f4d863e969ff7cbbda8661a15a705d7bf70c0e3
dkuenzel <45232239+dkuenzel@users.noreply.github.com>
2019-01-14 17:01:51 +0100

    documentation

--- app/api/routes.py
+++ app/api/routes.py
@@ -29,7 +29,6 @@ app.url_map.converters['float'] = FloatConverter
 
 ## Dynamic Routes
 # Output documentation with sandbox for requests on root
-
 @app.route('/')
 def help():
 	return send_from_directory(config.static_url_path + '/documentation', 'index.html')
@@ -46,6 +45,9 @@ def getStaticContent(topDir, subDir, fileName):
 def getStaticContentFromSubDir(topDir, subDir, subSubDir, fileName):
 	return send_from_directory(config.static_url_path + f"/{topDir}" + f"/{subDir}" + f"/{subSubDir}", fileName)
 
+
+## API Routes
+# TODO: Implement option to select if public transport lines should be included
 ## P2P Routing
 # Try: http://127.0.0.1:5000/p2p/0/5.125/51.31234/5.12/51.31
 @app.route('/p2p/0/<float:origin_lon>/<float:origin_lat>/<float:destination_lon>/<float:destination_lat>', methods=['GET'])
